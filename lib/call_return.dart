8a00d034b5e6cd98b127d0baf64ed41f3afb07e1
Chatmongcol <cdadwngs@gmail.com>
2023-09-01 11:33:06 +0700

    first commit

--- lib/call_return.dart
+++ lib/call_return.dart
@@ -1,15 +1,14 @@
-
-callReturn() {
-  // dynamic message = returnFun("Mr.Jeff Bezos",65);
-  List message= ["Mr.Jeff Bezos",65, true];
-  print(message);
+callReturn(){
+  // dynamic message = returnFun("Mr.Jeff Bezos", 65);
+  List massage=["Mr.Jeff Bezos", 65,true];
+  print(massage);
 }
 
-(String,int)returnFun(String reName, int reAge, bool reMarry){
-  String name ="Mr.Jeff Bezos";
-  int age =65;
-  bool mary= (name, age, marry);
-  var result = (name,age);
+(String,int, bool) returnFun(String reName, int reAge, bool reMarry){
+  String name = reName;
+  int age = reAge;
+  bool marry = reMarry;
+  var result = (name, age, marry);
   return result;
-  //print("Name:$name, Age:$age");
+  // print("Name:$name, Age:$age");
 }
